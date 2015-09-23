121783e330f0394ec3fe6d1f68625efd6f127ec8
Fabian Vogelsteller <fabian@frozeman.de>
2015-09-23 10:45:16 +0200

    add files

---
+++ packages/import/package.js
@@ -0,0 +1,28 @@
+Package.describe({
+  name: 'test:import',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.0.1');
+  api.use('ecmascript');
+  api.use('less');
+
+  // api.addFiles('otherfile.less', 'client'); // if you try to add the file, @myvar from main.less is not available 
+  api.addFiles('main.less', 'client');
+  api.addFiles('import.js');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('test:import');
+  api.addFiles('import-tests.js');
+});
